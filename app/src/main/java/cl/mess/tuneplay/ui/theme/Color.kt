f7136e60ccb2993b3021ef6f1abf4057574612d3
Mario Sánchez S. <marsanchezs@gmail.com>
2025-03-29 09:34:06 -0300

    add colors, strings and themes

--- app/src/main/java/cl/mess/tuneplay/ui/theme/Color.kt
+++ app/src/main/java/cl/mess/tuneplay/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package cl.mess.tuneplay.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(color = 0xFFD0BCFF)
+val PurpleGrey80 = Color(color = 0xFFCCC2DC)
+val Pink80 = Color(color = 0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(color = 0xFF6650a4)
+val PurpleGrey40 = Color(color = 0xFF625b71)
+val Pink40 = Color(color = 0xFF7D5260)
+
+val backgroundColor = Color(color = 0xFF121212)
+val itemColor = Color(color = 0xFF1E1E1E)
+val controlBarColor = Color(color = 0xFF252525)
+val highlightItemColor = Color(color = 0xFF343434)
+val highlightItemColorDarker = Color(color = 0xFF272727)
+val highlightItemColorLighter = Color(color = 0xFF4A4A4A)
